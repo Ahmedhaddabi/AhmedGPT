27b57a421956a98649870459413053c473bccdf0
Ahmed <alhaddabiahmed21@gmail.com>
2023-06-20 14:48:54 +0400

    first commit

--- client/script.jsx
+++ client/script.jsx
@@ -81,7 +81,7 @@ const handleSubmit = async (e) => {
   // messageDiv.innerHTML = "..."
   loader(messageDiv);
 
-  const response = await fetch('https://ahmedgpt.ahmedhaddabi.com/', {
+  const response = await fetch('https://ahmedgpt2.vercel.app/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
